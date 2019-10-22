829458984de2f34193886a2e3e4056771edd5eac
victor akpokiro <victorakpokiro@gmail.com>
2019-10-22 16:32:01 +0100

    work in progress

--- applib/backend/admin.py
+++ applib/backend/admin.py
@@ -1,12 +1,12 @@
 
-from flask import (Blueprint, url_for, request, render_template)
+from flask import (Blueprint, url_for, request, render_template, send_from_directory)
 
 from applib.lib import helper  as h
-from applib import forms as fm 
+from applib.backend import forms as fm 
 from applib import model as m 
 import base64
-
-
+import os
+from PIL import Image
 # +-------------------------+-------------------------+
 # +-------------------------+-------------------------+
 
@@ -20,26 +20,32 @@ app = Blueprint('admin', __name__, url_prefix='/backend')
 @app.route('/add', methods=['POST', 'GET'])
 def add():
 
+	form = fm.Service(request.form)
 	if request.method == 'POST':
 
-		image_file = request.files['service_image']
+		if form.service_image.data:
+			image_file = request.files[form.image.name]
+	
+			UPLOAD_FOLDER = 'applib/media'
+			_path = os.path.join(UPLOAD_FOLDER, image_file.filename)
+			image_file.save(_path)
+
+			params = {
+				'name': form.service_name.data,
+				'label': form.service_label.data,
+				'image': _path
+			}
+
+			with m.sql_cursor() as db:
+				services = m.Services(**params)
+				db.add(services)
 		
-		encoded_string = base64.b64encode(image_file.read())
-		image_file = encoded_string.decode('utf-8')
 
-		params = {
-			'name': request.form['service_name'],
-			'label': request.form['service_label'],
-			'image': image_file
-		}
+	return render_template('admin.html', form=form)
 
-		with m.sql_cursor() as db:
-			services = m.Services(**params)
-			db.add(services)
-		
 
-	return render_template('admin.html')
-
+# +-------------------------+-------------------------+
+# +-------------------------+-------------------------+
 
 
 @app.route('/list', methods=['POST', 'GET'])
@@ -57,3 +63,41 @@ def list():
 # +-------------------------+-------------------------+
 # +-------------------------+-------------------------+
 
+
+@app.route('/edit/<int:service_id>/', methods=['POST', 'GET'])
+def edit(service_id):
+	with m.sql_cursor() as db:
+		param = {'id': service_id}
+
+		data = db.query(m.Services.name,
+						m.Services.label,
+						m.Services.image).filter_by(**param)
+		data = data.first()
+
+		# form = fm.Service() 
+		# request.form['service_name'] = data.name
+		# request.form['service_label'] = data.label
+		# form.service_image.data = image.show()
+		# # import pudb
+		# # pudb.set_trace()
+		UPLOAD_FOLDER = 'applib/media'
+		# _path = data.image
+		# image = Image.open(_path)
+
+		image = send_from_directory(UPLOAD_FOLDER,
+                               filename= data.image, as_attachment=True)
+
+		if request.method == 'POST':
+			image_file = request.files['service_image']
+	
+			UPLOAD_FOLDER = 'applib/media'
+			_path = os.path.join(UPLOAD_FOLDER, image_file.filename)
+			image_file.save(_path)
+			data.update(
+				{
+					'name': request.form['service_name'],
+					'label': request.form['service_label'],
+					'image': _path
+				})
+
+	return render_template('edit.html', data=data, image=image)
--- applib/backend/forms.py
+++ applib/backend/forms.py
@@ -2,7 +2,7 @@ from wtforms import Form, validators, Field
 from wtforms.fields import (BooleanField, StringField, 
 							TextField, SubmitField, DateField, 
 							IntegerField, TextAreaField, SelectField,
-							HiddenField, DateTimeField,PasswordField)
+							HiddenField, DateTimeField, PasswordField, FileField)
 from wtforms.validators import input_required, Email, Length, ValidationError
 
 from wtforms import form, validators, fields
@@ -27,4 +27,12 @@ class LoginForm(Form):
 	psd_wrd = PasswordField("Password", [input_required()],
 							render_kw={"class_": "form-control"})
 		 
- 
+class Service(Form):
+	
+	service_name = StringField("Name", [input_required()], 
+							render_kw={"class_": "form-control"})
+	service_label = StringField("Label", [input_required()], 
+							render_kw={"class_": "form-control"})
+	service_image = FileField(u'Image File', [input_required()], 
+							render_kw={"class_": "form-control"})
+	
