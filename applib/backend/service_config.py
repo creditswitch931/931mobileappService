ac1d3fc3ab89da50ff500722ea8ce40755cc3ca3
victor akpokiro <victorakpokiro@gmail.com>
2019-10-23 09:51:15 +0100

    work in progress

--- applib/backend/service_config.py
+++ applib/backend/service_config.py
@@ -5,7 +5,6 @@ from flask import (Blueprint, url_for, request,
 from applib.lib import helper  as h
 from applib.backend import bk_form as fm 
 from applib import model as m 
-import base64
 import os
 
 
@@ -39,7 +38,8 @@ def add():
             params = {
                 'name': form.service_name.data,
                 'label': form.service_label.data,
-                'image': _path
+                'image': _path,
+                'category_name': form.category_name.data
             }
 
             with m.sql_cursor() as db:
@@ -79,7 +79,7 @@ def edit(service_id):
          
     form = fm.Service(**request.form)
 
-    if request.method == 'POST':
+    if request.method == 'POST' and form.validate():
 
         image_file = request.files['service_image']
                     
@@ -90,11 +90,12 @@ def edit(service_id):
             qry = db.query(m.ServicesMd).get(service_id)
             qry.name = form.service_name.data 
             qry.label = form.service_label.data
+            qry.category_name = form.category_name.data
             qry.image = _path
 
             db.add(qry)
 
-        return redirect(url_for("bk_cfg.list"))
+        return redirect(url_for("bk_cfg.service_view"))
 
 
     with m.sql_cursor() as db:
@@ -102,16 +103,18 @@ def edit(service_id):
 
         data = db.query(m.ServicesMd.name,
                         m.ServicesMd.label,
-                        m.ServicesMd.image).filter_by(**param).first()
+                        m.ServicesMd.image,
+                        m.ServicesMd.category_name).filter_by(**param).first()
 
         # temporary fix, to be cleaned up later 
 
         form.service_name.data = data.name 
-        form.service_label.data = data.label 
-
+        form.service_label.data = data.label
+        form.category_name.data = data.category_name 
  
 
         image = "/" + "/".join(data.image.split("/")[1:])
+       
 
 
 
--- applib/backend/web_login.py
+++ applib/backend/web_login.py
@@ -1,9 +1,10 @@
 
-from flask import (Blueprint, url_for, request, render_template)
-
+from flask import (Blueprint, url_for, request, render_template, redirect, flash)
+from applib.api.resp_handler import Response, RequestHandler, FormHandler
 from applib.lib import helper  as h
 from .bk_form import LoginForm
 from werkzeug.security import check_password_hash, generate_password_hash
+import json
 
 # +-------------------------+-------------------------+
 # +-------------------------+-------------------------+
@@ -13,7 +14,8 @@ app = Blueprint('backend', __name__, url_prefix='/backend')
 # +-------------------------+-------------------------+
 # +-------------------------+-------------------------+
 
-@app.route("/login", methods=['POST'])
+
+@app.route("/login", methods=['POST', 'GET'])
 def login():
 
 	form = LoginForm(request.form)
@@ -23,8 +25,25 @@ def login():
 		username = form.usr_name.data
 		password = form.psd_wrd.data
 
-		
+		url = h.get_config("API", "login")
+		login_content = {"username": username, "password": password, 
+							"mac_address":"00:24:D7:2B:55:64"}
+		req = RequestHandler(url, method=1, data=login_content)
+		resp = req.send()
 
+		for key, value in resp[1].items():
+			if key == "statusCode":
+				if value == "00":
+					continue
+			if key == "is_admin":
+				if value == "1":
+					return redirect(url_for('bk_cfg.service_view'))
+				else:
+					error = 'Incorrect Username/Password'
+					flash(error)
+					return redirect(url_for('.login'))
+
+				
 	return render_template('login.html', form=form)
 
 
@@ -34,7 +53,7 @@ def logout_app():
 	
 	# perform proper logout later on 
 
-    return redirect(url_for('  .login'))
+	return redirect(url_for('  .login'))
 
 
 
